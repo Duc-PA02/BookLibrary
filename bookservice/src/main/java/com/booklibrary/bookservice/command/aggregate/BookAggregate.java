492473ac81b48245fc8e5520a9937f8776c3ac30
PHAN DUC <ducpa2002@gmail.com>
2024-09-28 19:53:21 +0700

    update and delete book

--- bookservice/src/main/java/com/booklibrary/bookservice/command/aggregate/BookAggregate.java
+++ bookservice/src/main/java/com/booklibrary/bookservice/command/aggregate/BookAggregate.java
@@ -1,7 +1,11 @@
 package com.booklibrary.bookservice.command.aggregate;
 
 import com.booklibrary.bookservice.command.command.CreateBookCommmand;
+import com.booklibrary.bookservice.command.command.DeleteBookCommand;
+import com.booklibrary.bookservice.command.command.UpdateBookCommand;
 import com.booklibrary.bookservice.command.event.BookCreatedEvent;
+import com.booklibrary.bookservice.command.event.BookDeletedEvent;
+import com.booklibrary.bookservice.command.event.BookUpdatedEvent;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,16 +28,44 @@ public class BookAggregate {
     private Boolean isReady;
 
     @CommandHandler
-    public void handle(CreateBookCommmand commmand){
+    public BookAggregate(CreateBookCommmand commmand){
         BookCreatedEvent bookCreatedEvent = new BookCreatedEvent();
         BeanUtils.copyProperties(commmand, bookCreatedEvent);
         AggregateLifecycle.apply(bookCreatedEvent);
     }
+
+    @CommandHandler
+    public void handle(UpdateBookCommand command){
+        BookUpdatedEvent bookUpdatedEvent = new BookUpdatedEvent();
+        BeanUtils.copyProperties(command,bookUpdatedEvent);
+        AggregateLifecycle.apply(bookUpdatedEvent);
+    }
+
+    @CommandHandler
+    public void handle(DeleteBookCommand command){
+        BookDeletedEvent bookDeletedEvent = new BookDeletedEvent();
+        BeanUtils.copyProperties(command,bookDeletedEvent);
+        AggregateLifecycle.apply(bookDeletedEvent);
+    }
+
     @EventSourcingHandler
     public void on(BookCreatedEvent event){
         this.id = event.getId();
         this.name = event.getName();
         this.author = event.getAuthor();
         this.isReady = event.getIsReady();
+    }
+
+    @EventSourcingHandler
+    public void on(BookUpdatedEvent event){
+        this.id = event.getId();
+        this.name = event.getName();
+        this.author = event.getAuthor();
+        this.isReady = event.getIsReady();
+    }
+
+    @EventSourcingHandler
+    public void on(BookDeletedEvent event){
+        this.id = event.getId();
     }
 }
--- bookservice/src/main/java/com/booklibrary/bookservice/command/event/BookEventsHandler.java
+++ bookservice/src/main/java/com/booklibrary/bookservice/command/event/BookEventsHandler.java
@@ -7,6 +7,8 @@ import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class BookEventsHandler {
@@ -17,5 +19,23 @@ public class BookEventsHandler {
         Book book = new Book();
         BeanUtils.copyProperties(event, book);
         bookRepository.save(book);
+    }
+
+    @EventHandler
+    public void on(BookUpdatedEvent event){
+        Optional<Book> oldBook = bookRepository.findById(event.getId());
+        oldBook.ifPresent(book -> {
+            book.setName(event.getName());
+            book.setAuthor(event.getAuthor());
+            book.setIsReady(event.getIsReady());
+            bookRepository.save(book);
+        });
+
+    }
+
+    @EventHandler
+    public void on(BookDeletedEvent event){
+        Optional<Book> oldBook = bookRepository.findById(event.getId());
+        oldBook.ifPresent(book -> bookRepository.delete((book)));
     }
 }
